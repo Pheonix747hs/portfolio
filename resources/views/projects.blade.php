dde5e735946cdb61aad28e208161457c532a8bbd
Pheonix747hs <94136752+Pheonix747hs@users.noreply.github.com>
2023-12-19 19:01:41 +0530

    2

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -7,9 +7,9 @@
                 </a>
                 <div class="p-5">
                     <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $card->title }}</h5>
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-gray-900">{{ $card->title }}</h5>
                     </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $card->info }}</p>
+                    <p class="mb-3 font-normal text-gray-300 dark:text-gray-400">{{ $card->info }}</p>
                     <a href="{{ $card->url }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Read more
                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
