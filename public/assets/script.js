2f104bdd8b141de0a158a5158ca026840fab13f7
Ojasvin Borawke <94136752+Pheonix747hs@users.noreply.github.com>
2023-12-20 01:41:44 +0530

    Update script.js

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,56 +1,47 @@
 // custom-script.js
-function yourCustomFunction() {
+$(document).ready(function () {
   var $customGrid = $('.grid2');
   var $gridUnit = $('.grid__item');
 
   var gridUnitHeight = $gridUnit.height();
   var gridUnitWidth = $gridUnit.width();
 
-  var horzCount = Math.ceil($customGrid.width() / gridUnitWidth) + 5;
-  var vertCount = Math.ceil($customGrid.height() / gridUnitHeight) + 5;
+  function addGridItems() {
+    var horzCount = Math.ceil($customGrid.width() / gridUnitWidth) + 5;
+    var vertCount = Math.ceil($customGrid.height() / gridUnitHeight) + 5;
 
-  var totalGridItems = horzCount * vertCount;
-  for (var i = 0; i < totalGridItems; i++) {
-    var $gridUnitClone = $gridUnit.clone();
-    $customGrid.append($gridUnitClone);
+    var totalGridItems = horzCount * vertCount;
+    for (var i = 0; i < totalGridItems; i++) {
+      var $gridUnitClone = $gridUnit.clone();
+      $customGrid.append($gridUnitClone);
+    }
   }
+
+  // Initial load
+  addGridItems();
+
+  // Add more grids as user scrolls down
+  $(window).on('scroll', function () {
+    var scrollPosition = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    var documentHeight = $(document).height();
+
+    // Adjust this value based on when you want to trigger more grids
+    var triggerPoint = 0.8; // 80% down the page
+
+    if (scrollPosition > (triggerPoint * (documentHeight - windowHeight))) {
+      addGridItems();
+    }
+  });
 
   // Add touch interaction to grid items
   $gridUnit.on('touchstart touchmove', function (e) {
-    e.preventDefault(); // Prevent default touch behavior
+    e.preventDefault();
 
-    // Get touch coordinates
-    var touchX = e.originalEvent.touches[0].clientX;
-    var touchY = e.originalEvent.touches[0].clientY;
-
-    // Check if the touch position is within the grid item
-    $gridUnit.each(function () {
-      var $this = $(this);
-      if (
-        touchX >= $this.offset().left &&
-        touchX <= $this.offset().left + $this.width() &&
-        touchY >= $this.offset().top &&
-        touchY <= $this.offset().top + $this.height()
-      ) {
-        $this.addClass('touch-hover');
-      } else {
-        $this.removeClass('touch-hover');
-      }
-    });
+    // Touch event handling code...
   });
 
-  // Remove touch-hover class when touch ends
   $gridUnit.on('touchend', function () {
     $(this).removeClass('touch-hover');
-  });
-}
-
-// Run the function on document ready
-$(document).ready(function () {
-  yourCustomFunction();
-
-  // Add click event listener to the button
-  $('#triggerButton').on('click', function () {
-    yourCustomFunction();
   });
 });
